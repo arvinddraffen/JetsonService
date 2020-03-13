1a4536f27e2b4170003b4fe5a600a723286bbcbc
Arvind Draffen <arvinddraffen@gmail.com>
2020-03-12 20:27:18 -0500

    Added ClusterContext class and sample code for submitting values to database with models

---
+++ JetsonService/Data/ClusterContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using JetsonModels;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace JetsonService.Data
+{
+    /// <summary>
+    /// <see cref="ClusterContext"/> provides an interface to the SQLite database with EntityFramework.
+    /// </summary>
+    public class ClusterContext : DbContext
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ClusterContext"/> class.
+        /// </summary>
+        /// <remarks>
+        /// Will create the SQLite database if not already created.
+        /// </remarks>
+        /// <param name="options"></param>
+        public ClusterContext (DbContextOptions<ClusterContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+
+        /// <summary>
+        /// Gets or sets the list of clusters (<see cref="Cluster"/>) which constitute the system.
+        /// </summary>
+        public DbSet<Cluster> Clusters { get; set; }
+
+        /// <inheritdoc/>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<NodeUtilization>()
+                .Property(x => x.Cores)
+                .HasConversion(
+                    v => JsonConvert.SerializeObject(v),
+                    v => JsonConvert.DeserializeObject<ICollection<CpuCore>>(v)
+                );
+        }
+    }
+}
--- JetsonService/Program.cs
+++ JetsonService/Program.cs
@@ -1,12 +1,70 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using JetsonModels;
+using JetsonService.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace JetsonService
 {
-    class Program
+    /// <summary>
+    /// <see cref="Program"/> is the main entry point into JetsonService.
+    /// </summary>
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var optionsBuilder = new DbContextOptionsBuilder<ClusterContext>();
+            optionsBuilder.EnableSensitiveDataLogging();
+            optionsBuilder.UseLazyLoadingProxies();
+            optionsBuilder.UseSqlite("Data Source=data.db");
+            var database = new ClusterContext(optionsBuilder.Options);
+
+            /* Sample code for providing an entry into the database */
+
+            // Find cluster with Id of 2. If not exists, create new cluster
+            var cluster = database.Clusters.Find((uint)2);
+            if (cluster == null)
+            {
+                cluster = new Cluster();
+                cluster.Id = 2;
+                cluster.Nodes = new List<Node>();
+                database.Clusters.Add(cluster);
+            }
+
+            // Find in cluster (cluster Id 2) with Id of 1. If not exists, create new node
+            var node = cluster.Nodes.FirstOrDefault(n => n.Id == 1);
+            if (node == null)
+            {
+                cluster.Nodes.Add(
+                  new Node()
+                  {
+                      Id = 1,
+                      IPAddress = "5.4.3.2",
+                      Power = new List<NodePower>(),
+                      Utilization = new List<NodeUtilization>(),
+                  });
+            }
+
+            // Add utilization information for the node (of Id 1)
+            node.Utilization.Add(new NodeUtilization()
+            {
+                MemoryAvailable = 5,
+                MemoryUsed = 100 * 1000,
+                TimeStamp = DateTime.Now,
+                Cores = new List<CpuCore>()
+                            {
+                                new CpuCore() { CoreNumber = 0, UtilizationPercentage = 1000 / 2 },
+                                new CpuCore() { CoreNumber = 1, UtilizationPercentage = 1000 + 1000 },
+                            },
+            });
+
+            // Add power use information for the node (of Id 1)
+            node.Power.Add(new NodePower() { Timestamp = DateTime.Now, Current = 1000, Voltage = 1000 });
+
+            // Save changes to the database
+            database.SaveChanges();
         }
     }
 }
