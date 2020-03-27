fe87244386473ea10ba28d1e2b6acb986f34ab18
Mateo Canabal <realmadridfan133@protonmail.com>
2020-03-27 15:16:33 -0500

    Wrote a new feature which receives JSON's from nodes whose IP's are specified in a config file of a struct with updated utilization data.

--- JetsonService/Program.cs
+++ JetsonService/Program.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Threading.Tasks;
 
 using JetsonModels;
 using JetsonService.Data;
 using Microsoft.EntityFrameworkCore;
+using RestSharp;
+
+struct UpdateMessage
+{
+    public uint CID, NID;   // Cluster ID, Node ID
+    public uint freemem, usedmem;   // MB
+    public String NIP;  // IPv4 address
+    public float[] cpu_util;    // %
+};
 
 namespace JetsonService
 {
@@ -13,84 +22,100 @@ namespace JetsonService
     /// </summary>
     internal class Program
     {
-        private static void Main(string[] args)
+        static List<string> NodeIPs = new List<string>();
+
+        static ClusterContext database;
+
+        private static readonly object myLock = new object();
+
+        private static void Init()
         {
-            uint sampleClusterId = 2;
+            string ConfigFile = System.IO.File.ReadAllText(@"JetsonServiceConfig.txt");
+            string[] SplitConfigFile = ConfigFile.Split(new Char[] { '\n' });
+            NodeIPs.AddRange(SplitConfigFile);
+        }
 
-            var x = new System.Diagnostics.Stopwatch();
-            x.Start();
-
-            var optionsBuilder = new DbContextOptionsBuilder<ClusterContext>();
-            optionsBuilder.EnableSensitiveDataLogging();
-            optionsBuilder.UseSqlite("Data Source=data.db");
-            var database = new ClusterContext(optionsBuilder.Options);
-
-            /* Sample code for providing an entry into the database */
-
-            // Find cluster with Id of 2. If not exists, create new cluster
-            var cluster = database.Clusters
-                .Include(c => c.Nodes)
-                .FirstOrDefault(c => c.Id == sampleClusterId);
-
-            if (cluster == null)
+        private static void ReceiveMessage(int index)
+        {
+            while(true)
             {
-                cluster = new Cluster();
-                cluster.Id = sampleClusterId;
-                cluster.Nodes = new List<Node>();
-                database.Clusters.Add(cluster);
-            }
+                var client = new RestClient("https://" + NodeIPs[index]);
+                var request = new RestRequest(Method.GET);
+                request.RequestFormat = DataFormat.Json;
+                var myMessage = client.Execute<UpdateMessage>(request).Data;    // Execute is synchronous, so it should block until request is received.
 
-            // Make 20 nodes
-            for (uint nodeIndex = 0; nodeIndex < 20; nodeIndex++)
-            {
-                //// Find in cluster (cluster Id 2) with Id of 1. If not exists, create new node
-                var node = cluster.Nodes.FirstOrDefault(n => n.Id == nodeIndex);
+                // Find cluster with received Id. If not exists, create new cluster
+                var cluster = database.Clusters
+                    .Include(c => c.Nodes)
+                    .FirstOrDefault(c => c.Id == myMessage.CID);
+
+                if (cluster == null)
+                {
+                    cluster = new Cluster();
+                    cluster.Id = myMessage.CID;
+                    cluster.Nodes = new List<Node>();
+                    database.Clusters.Add(cluster);
+                }
+
+                //// Find in cluster with given node Id. If not exists, create new node
+                var node = cluster.Nodes.FirstOrDefault(n => n.Id == myMessage.NID);
 
                 if (node == null)
                 {
                     node = new Node()
                     {
-                        Id = nodeIndex,
-                        IPAddress = $"5.4.3.{nodeIndex}",
+                        Id = myMessage.NID,
+                        IPAddress = myMessage.NIP,
                     };
                     cluster.Nodes.Add(node);
                 }
 
-                database.SaveChanges();
-            }
-
-            // add some data for 7 days
-            for (int entry = 0; entry < 1 * 60 * 60 * 24; entry++)
-            {
-                foreach (var node in cluster.Nodes)
+                lock (myLock)
                 {
-                    WriteUtilizationData(database, node.GlobalId, entry);
+                    database.SaveChanges();
+                    WriteUtilizationData(node.GlobalId, myMessage);
+                    database.SaveChanges();
                 }
             }
-
-            database.SaveChanges();
-
-            x.Stop();
-            Console.WriteLine($"Completed test build in {x.Elapsed.TotalSeconds} sec");
         }
 
-        private static void WriteUtilizationData(ClusterContext database, uint globalNodeId, int i)
+        private static void Main(string[] args)
         {
-            // Add utilization information for the node (of Id 1)
+            Init();
+
+            var optionsBuilder = new DbContextOptionsBuilder<ClusterContext>();
+            optionsBuilder.EnableSensitiveDataLogging();
+            optionsBuilder.UseSqlite("Data Source=data.db");
+            database = new ClusterContext(optionsBuilder.Options);
+
+            Task[] myTasks = new Task[NodeIPs.Count];
+            for (int i = 0; i < NodeIPs.Count; i++)
+            {
+                myTasks[i] = Task.Factory.StartNew(() => ReceiveMessage(i));
+            }
+            Task.WaitAll(myTasks);
+        }
+
+        private static void WriteUtilizationData(uint globalNodeId, UpdateMessage myMessage)
+        {
+            List<CpuCore> myCores = new List<CpuCore>();
+
+            for (uint i = 0; i < myMessage.cpu_util.Length; i++)
+            {
+                myCores.Add(new CpuCore() { CoreNumber = i, UtilizationPercentage = myMessage.cpu_util[i] });
+            }
+
+            // Add utilization information for the node
             database.UtilizationData.Add(new NodeUtilization()
             {
                 GlobalNodeId = globalNodeId,
-                MemoryAvailable = 5,
-                MemoryUsed = 100 * 1000,
+                MemoryAvailable = myMessage.freemem,
+                MemoryUsed = myMessage.usedmem,
                 TimeStamp = DateTime.Now,
-                Cores = new List<CpuCore>()
-                        {
-                            new CpuCore() { CoreNumber = 0, UtilizationPercentage = i / 2 },
-                            new CpuCore() { CoreNumber = 1, UtilizationPercentage = i % 7500 },
-                        },
+                Cores = myCores,
             });
 
-            // Add power use information for the node (of Id 1)
+            // Add power use information for the node
             database.PowerData.Add(new NodePower() { GlobalNodeId = globalNodeId, Timestamp = DateTime.Now, Current = ((float)i / (float)3) % 744, Voltage = ((float)i / (float)4) % 4, Power = (((float)i / (float)1000)) * (((float)i / (float)2000)) });
         }
     }
